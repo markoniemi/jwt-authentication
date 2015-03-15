54e10f972d08e76470c9fd6baaaceac43dc71408
niemimac <marko.j.niemi@gmail.com>
2015-03-15 19:39:40 +0200

    ErrorMapper improved.

--- jwt-authentication/src/main/java/org/jwt/service/ErrorMapper.java
+++ jwt-authentication/src/main/java/org/jwt/service/ErrorMapper.java
@@ -11,42 +11,28 @@ import javax.ws.rs.ext.Provider;
 
 import lombok.extern.slf4j.Slf4j;
 
-// TODO move to org.jwt
+/**
+ * Jersey specific ExceptionMapper. Rest services can throw exceptions, and
+ * mapper maps them to proper responses.
+ */
 @Provider
 @Slf4j
 public class ErrorMapper implements ExceptionMapper<Exception> {
 
-	@Override
-	public Response toResponse(Exception exception) {
-		return createResponse(createErrorMessage(exception));
-	}
+    @Override
+    public Response toResponse(Exception exception) {
+        return createResponse(exception);
+    }
 
-	private Response createResponse(ErrorMessage errorMessage) {
-		return Response
-				.status(errorMessage.getStatus())
-				.entity(errorMessage.getMessage())
-				.type(MediaType.TEXT_PLAIN)
-				.header(errorMessage.getHeaderName(),
-						errorMessage.getHeaderValue()).build();
-	}
-
-	private ErrorMessage createErrorMessage(Exception exception) {
-		ErrorMessage errorMessage = new ErrorMessage();
-		if (exception instanceof WebApplicationException) {
-			errorMessage.setStatus(((WebApplicationException) exception)
-					.getResponse().getStatus());
-			errorMessage.setMessage(exception.getMessage());
-		} else if (exception instanceof AuthenticationException || exception instanceof ServletException) {
-			errorMessage.setStatus(Status.UNAUTHORIZED.getStatusCode());
-			errorMessage.setMessage(exception.getMessage());
-			errorMessage.setHeaderName("WWW-Authenticate");
-			errorMessage.setHeaderValue("JWT");
-		} else {
-			errorMessage.setStatus(Response.Status.INTERNAL_SERVER_ERROR
-					.getStatusCode());
-			errorMessage.setMessage(exception.getMessage());
-		}
-		log.debug(errorMessage.toString());
-		return errorMessage;
-	}
+    private Response createResponse(Exception exception) {
+        if (exception instanceof WebApplicationException) {
+            return Response.status(((WebApplicationException) exception).getResponse().getStatus())
+                    .entity(exception.getMessage()).type(MediaType.TEXT_PLAIN).build();
+        } else if (exception instanceof AuthenticationException || exception instanceof ServletException) {
+            return Response.status(Status.UNAUTHORIZED.getStatusCode()).entity(exception.getMessage())
+                    .type(MediaType.TEXT_PLAIN).header("WWW-Authenticate", "JWT").build();
+        }
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()).entity(exception.getMessage())
+                .type(MediaType.TEXT_PLAIN).build();
+    }
 }
