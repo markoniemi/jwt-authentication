9faa5d82769d1320f320d4f57c00a53f82354cef
niemimac <marko.j.niemi@gmail.com>
2015-04-07 19:34:09 +0300

    Fixes #6: move authInterceptor to authInterceptor.js.

--- jwt-authentication/src/main/javascript/app/app.js
+++ jwt-authentication/src/main/javascript/app/app.js
@@ -1,25 +1,2 @@
 var app = angular.module('jwt-authentication', [ 'ngResource', 'ngRoute',
 		'pascalprecht.translate', 'ui.bootstrap', 'ngStorage', 'angular-jwt' ]);
-
-app.factory('authInterceptor', function ($q, $localStorage, $location, $log) {
-    return {
-        request: function (config) {
-        	if($localStorage.authenticationToken) {
-        		$log.debug("sending authenticationToken");
-            	config.headers['Authorization'] = 'Bearer ' + $localStorage.authenticationToken;
-            }
-            return config;
-        },
-        responseError: function (response) {
-            if(response.status === 401) {
-            	$log.debug("401");
-            }
-            $location.path('/login/login');
-            return $q.reject(response);
-        }
-    };
-});
-
-app.config(function ($httpProvider) {
-    $httpProvider.interceptors.push('authInterceptor');
-});
